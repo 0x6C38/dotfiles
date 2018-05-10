d8e0d824fb3d24eb9e45d5b159a3d957db951427
Danilo Salvio <danilo_salvio@live.com>
2018-05-10 00:05:29 -0300

    Adds nix main configuration file.

---
+++ dotfiles/configuration.nix
@@ -0,0 +1,124 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  # boot.loader.grub.efiSupport = true;
+  # boot.loader.grub.efiInstallAsRemovable = true;
+  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  # Define on which hard drive you want to install Grub.
+  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+
+  # networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Select internationalisation properties.
+  i18n = {
+  #  consoleFont = "Lat2-Terminus16";
+  #  consoleKeyMap = "la-latin1";
+  #  defaultLocale = "en_US.UTF-8";
+     inputMethod = {
+       enabled = "fcitx";
+       fcitx.engines = with pkgs.fcitx-engines; [mozc];
+     };
+  };
+
+  # Set your time zone.
+  time.timeZone = "America/Argentina/Buenos_Aires";
+
+  fonts = {
+    #enableFontDir = true;
+    #enableGhostscriptFonts = true;
+    fonts = with pkgs; [
+      powerline-fonts
+    ];
+  };
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    wget curl
+    vim
+    termite
+    git
+    firefox
+    python python36Packages.pip
+    nox
+    feh imagemagick neofetch
+    nodejs-9_x
+    oh-my-zsh zsh
+    numlockx
+  ];
+
+  programs.zsh.enable = true;
+  programs.zsh.interactiveShellInit = ''
+    export ZSH=${pkgs.oh-my-zsh}/share/oh-my-zsh/
+
+    # Customize your oh-my-zsh options here
+    ZSH_THEME="agnoster"
+    plugins=(git)
+  
+    source $ZSH/oh-my-zsh.sh
+  '';
+
+  programs.zsh.promptInit = ""; # Clear this to avoid a conflict with oh-my-zsh
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.bash.enableCompletion = true;
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.layout = "latam";
+  #services.xserver.xkbOptions = "eurosign:e";
+  services.xserver.dpi = 157;
+
+  services.xserver.windowManager.i3.enable = true;
+  #services.xserver.windowManager.i3.i3-gaps = true;
+
+  # Enable the KDE Desktop Environment.
+  # services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.desktopManager.plasma5.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.extraUsers.ds = {
+     isNormalUser = true;
+     home = "/home/ds";
+     extraGroups = ["wheel" "networkmanager"];
+     uid = 1000;
+     shell = pkgs.zsh;
+  };
+  users.extraUsers.root = {
+    shell = pkgs.zsh;
+  };
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "18.03"; # Did you read the comment?
+
+}
